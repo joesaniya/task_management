83687ea78462aa6217aaee7df3bcaa1b347d5ecb
Esther Jenslin <esther@katenterprise.com>
2025-02-01 18:23:54 +0530

    dead code removed

--- lib/provider/task_provider.dart
+++ lib/provider/task_provider.dart
@@ -1,6 +1,4 @@
 import 'dart:async';
-import 'dart:developer';
-
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
